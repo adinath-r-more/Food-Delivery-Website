212772b066a079f3191b99dd1fe1292d0d51742b
Adinath more <moreadi7777@gmail.com>
2025-11-17 18:30:38 +0530

    website not woriking

--- script.js
+++ script.js
@@ -16,12 +16,13 @@ const cartValue = document.querySelector('.cart-value');
 const hamburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-menu');
 const bars = document.querySelector('.fa-bars');
+hamburgerOne = document.querySelector('.fa-times')
 
 cartIcon.addEventListener('click', () => cartTab.classList.add('cart-tab-active'));
 closeBtn.addEventListener('click', () => cartTab.classList.remove('cart-tab-active'));
 hamburger.addEventListener('click', () => mobileMenu.classList.toggle('mobile-menu-active'));
 hamburger.addEventListener('click', () => bars.classList.remove('fa-bars'));
-hamburger.addEventListener('click', () => bars.classList.add('fa-times'));
+hamburgerOne.addEventListener('click', () => bars.classList.add('fa-times'));
 
 let ProductList = [];
 let cartProduct = [];
